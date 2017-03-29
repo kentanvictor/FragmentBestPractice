aef60320b3b7423d2e82508200b17882552164cd
kentanvictor <2030457926@qq.com>
2017-03-29 20:19:33 +0800

    README

---
+++ app/src/main/java/com/example/dell/fragmentbestpractice/News.java
@@ -0,0 +1,26 @@
+package com.example.dell.fragmentbestpractice;
+
+/**
+ * Created by DELL on 2017/3/28.
+ */
+
+public class News {
+    private String title;
+    private String content;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
