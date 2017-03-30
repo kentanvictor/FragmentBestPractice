0a436fac6dbd337211c263928ae022cf6c8e3a31
kentanvictor <2030457926@qq.com>
2017-03-30 15:37:23 +0800

    p163(unable)->code

---
+++ app/src/main/java/com/example/dell/fragmentbestpractice/NewsContentActivity.java
@@ -0,0 +1,21 @@
+package com.example.dell.fragmentbestpractice;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NewsContentActivity extends AppCompatActivity {
+public static void actionStart(Context context,String newsTitle,String newsContent)
+{
+    Intent intent = new Intent(context,NewsContentActivity.class);
+    intent.putExtra("news_title",newsTitle);
+    intent.putExtra("news_content",newsContent);
+    context.startActivity(intent);
+}
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.news_content);
+    }
+}
