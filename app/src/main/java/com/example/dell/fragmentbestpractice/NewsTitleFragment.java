8e069bb6c8944bbf7b4507890706ab99b97aa8b5
kentanvictor <2030457926@qq.com>
2017-04-01 11:02:57 +0800

    p1166->code(unable)

--- app/src/main/java/com/example/dell/fragmentbestpractice/NewsTitleFragment.java
+++ app/src/main/java/com/example/dell/fragmentbestpractice/NewsTitleFragment.java
@@ -2,9 +2,13 @@ package com.example.dell.fragmentbestpractice;
 
 import android.app.Fragment;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
 
 /**
  * Created by DELL on 2017/3/31.
@@ -29,6 +33,28 @@ public class NewsTitleFragment extends Fragment{
         else
         {
             isTwoPane = false;//找不到的时候，为单页模式
+        }
+    }
+    class NewsAdapter extends RecyclerView.Adapter<NewsAdapter.ViewHolder>
+    {
+        private List<News> mNewsList;
+        class ViewHolder extends RecyclerView.ViewHolder
+        {
+            TextView newsTitleText;
+            public ViewHolder (View view)
+            {
+                super(view);
+                newsTitleText = (TextView)view.findViewById(R.id.news_title);
+            }
+        }
+        public NewsAdapter (List<News> newsList)
+        {
+            mNewsList = newsList;
+        }
+        @Override
+        public ViewHolder onCreateViewHolder(ViewGroup parebt,int viewType)
+        {
+            View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.news_item,parent,false);
         }
     }
 }
